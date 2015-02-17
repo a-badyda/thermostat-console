e3502fc7d57935dc04e8f63adabbac5e182f8521
Aleksandra Badyda <a.badyda@gmail.com>
2015-02-17 15:54:54 +0000

    more porting about
    the tabs finally work switched to just jQuery UI

--- src/testing.php
+++ src/testing.php
@@ -1,37 +1,53 @@
-<?php
-
-
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-    
-    <head>
- 
-        <!-- Style Sheets --> 
-        <link rel="stylesheet" type="text/css" href="libraries/css/jquery.mobile.custom.structure.min.css" /> 
-		<link rel="stylesheet" type="text/css" href="libraries/css/jquery.mobile.custom.theme.min.css" /> 
+	<head>
+		<meta charset="utf-8">
 
 		<!-- 
        special meta tags should be added into HTML that is supposed to be 
        loaded on mobile devices 
-    -->
+    	-->
     	<meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=0"/>
     	<meta name="msapplication-tap-highlight" content="no" />
- 
-        
-        <!-- Scripts --> 
-        <script type="text/javascript" src="libraries/jquery-1.11.2.js"></script>
-        <script type="text/javascript" src="libraries/flotr2.min.js"></script>
-        <script type="text/javascript" src="libraries/jquery.mobile.custom.min.js"></script>
-        
+	  	
+	  	<title>jQuery UI Tabs - Default functionality</title>
+	  	<link rel="stylesheet" href="libraries/css/jquery-ui.css">
+	  	
+	  	<script src="libraries/jquery-1.11.2.js"></script>
+	  	<script src="libraries/jquery-ui.js"></script>
 
-        <Title>TESTING</Title>
-        
-    </head>
+		<script>
+			$(function() {
+				$( "#tabs" ).tabs();
+		  	});
+		</script>
 
-    <script>
-	   
+	</head>
+	<body>
+	 
+		<div id="tabs">
+		  	<ul>
+				<li><a href="#stats">Statistics</a></li>
+				<li><a href="#heat">Heating Control</a></li>
+				<li><a href="#book">Room Booking</a></li>
+				<li><a href="#login">User Login</a></li>
+		  	</ul>
 
-	</script>
+		  	<div id="stats">
+				<p> hello friends </p>
+			</div>
+
+		  	<div id="heat">
+				<p> What is up </p>
+			</div>
+
+		  	<div id="book">
+				<p> why </p>
+			</div>
+
+		</div>
+	 
+	 
+	</body>
+
 </html>
