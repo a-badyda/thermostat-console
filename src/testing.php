ce3d6a6bf74eae970514e576b556000f2d522d0a
Aleksandra Badyda <a.badyda@gmail.com>
2015-02-17 14:00:12 +0000

    Replaced Zebra with JQuery Mobile
    Better Suited for actually getting things done on mobile, plus used
    jquery UI before, more likely to handle it well. extra images for jQuery

--- src/testing.php
+++ src/testing.php
@@ -11,8 +11,13 @@
         <!-- Style Sheets 
         <link rel="stylesheet" type="text/css" href="main/style.css" /> 
 		-->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-        <meta name="msapplication-tap-highlight" content="no" />
+		<!-- 
+       special meta tags should be added into HTML that is supposed to be 
+       loaded on mobile devices 
+    -->
+    	<meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=0"/>
+    	<meta name="msapplication-tap-highlight" content="no" />
+ 
         
         <!-- Scripts --> 
         <script type="text/javascript" src="libraries/jquery-1.11.2.js"></script>
@@ -26,16 +31,22 @@
 
     <script>
 	    zebra.ready(function() {
-	    	// create canvas 400x700 pixels
-	    	var canvas = new zebra.ui.zCanvas(400, 700);
-	 
-	    	// fill canvas root panel with UI components
-	    	canvas.root.setLayout(new zebra.layout.BorderLayout(8));
-	    	canvas.root.add(zebra.layout.CENTER, new zebra.ui.TextArea(""));
-	    	canvas.root.add(zebra.layout.BOTTOM, new zebra.ui.Button("Clean"));
-
-	    	// force canvas to fill all available page space
-     		canvas.fullScreen();  
+	    	// create canvas and set border layout for root panel
+			var r = new zebra.ui.zCanvas(150, 250).root;
+			r.setLayout(new zebra.layout.BorderLayout());
+			 
+			// create list UI component
+			var list = new zebra.ui.List();
+			 
+			// add 100 items to the UI list component
+			for(var i=0; i<100; i++) {
+			   list.model.add("List item " + i);
+			}
+			 
+			// add scroll panel to root panel and add 
+			// list to the scroll panel  
+			r.add(zebra.layout.CENTER, 
+			      new zebra.ui.ScrollPan(list));
 		});
 
 	</script>
