34c1abce3a2206176c334ac5594b618f436ad6a2
Aleksandra Badyda <a.badyda@gmail.com>
2015-02-17 14:11:42 +0000

    moving things around
    placement of stuff (images folder) and adjusting testing.php to begin
    testing out jquery mobile

--- src/testing.php
+++ src/testing.php
@@ -8,9 +8,10 @@
     
     <head>
  
-        <!-- Style Sheets 
-        <link rel="stylesheet" type="text/css" href="main/style.css" /> 
-		-->
+        <!-- Style Sheets --> 
+        <link rel="stylesheet" type="text/css" href="libraries/css/jquery.mobile.custom.structure.min.css" /> 
+		<link rel="stylesheet" type="text/css" href="libraries/css/jquery.mobile.custom.theme.min.css" /> 
+
 		<!-- 
        special meta tags should be added into HTML that is supposed to be 
        loaded on mobile devices 
@@ -21,8 +22,8 @@
         
         <!-- Scripts --> 
         <script type="text/javascript" src="libraries/jquery-1.11.2.js"></script>
-        <script type="text/javascript" src="libraries/zebra.runtime/zebra.min.js"></script>
         <script type="text/javascript" src="libraries/flotr2.min.js"></script>
+        <script type="text/javascript" src="libraries/jquery.mobile.custom.min.js"></script>
         
 
         <Title>TESTING</Title>
@@ -30,24 +31,7 @@
     </head>
 
     <script>
-	    zebra.ready(function() {
-	    	// create canvas and set border layout for root panel
-			var r = new zebra.ui.zCanvas(150, 250).root;
-			r.setLayout(new zebra.layout.BorderLayout());
-			 
-			// create list UI component
-			var list = new zebra.ui.List();
-			 
-			// add 100 items to the UI list component
-			for(var i=0; i<100; i++) {
-			   list.model.add("List item " + i);
-			}
-			 
-			// add scroll panel to root panel and add 
-			// list to the scroll panel  
-			r.add(zebra.layout.CENTER, 
-			      new zebra.ui.ScrollPan(list));
-		});
+	   
 
 	</script>
 </html>
