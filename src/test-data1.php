35f71dd29cae3c8893f7e25a7e857253110b2378
Aleksandra Badyda <a.badyda@gmail.com>
2015-03-04 12:39:46 +0000

    added a new file for outside temperatures

---
+++ src/test-data1.php
@@ -0,0 +1,30 @@
+<?php
+// The JSON standard MIME header.
+header('Content-type: application/json');
+
+//some fake temperature data to send over to the server
+$temperatureData = array( 
+	array(0, -3),
+	array(1, -1),
+	array(2, 3),
+	array(3, 6),
+	array(4, 10),
+	array(5, 15),
+	array(6, 16),
+	array(7, 14),
+	array(8, 10),
+	array(9, 5),
+	array(10, 6),
+	array(11, 10),
+	array(12, 15),
+	array(13, 16),
+	array(14, 15),
+	array(15, 16),
+	array(16, 14)
+	);
+
+// Send the data.
+print_r(json_encode($temperatureData));
+
+
+?>
